48600a017f2c1cd1263781435a14bd822386f339
abhishek-2k23 <kumarabhishek00090@gmail.com>
2025-04-04 02:52:35 +0530

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
 import dbConnection from "./configuration/db.js";
+import router from './routers/router.js';
+import cors from 'cors';
+
+dotenv.config();
 
 const app = express();
-dbConnection();
+app.use(express.json());
+app.use(cors())
+
+//connect with database
+await dbConnection();
 
 app.listen(process.env.PORT, () => {
     console.log("Server is up at ", process.env.PORT);
@@ -13,3 +20,6 @@ app.listen(process.env.PORT, () => {
 app.get("/", (_, res) => {
     res.send("restaurant booking server")
 })
+
+//mounting routes
+app.use('/api', router);
