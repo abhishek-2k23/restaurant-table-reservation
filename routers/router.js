b2b138adc3d4cdd03fbc872ddd81f807861fa1bb
abhishek-2k23 <kumarabhishek00090@gmail.com>
2025-04-06 03:34:28 +0530

    add restaurant and resevervation routes

--- routers/router.js
+++ routers/router.js
@@ -2,8 +2,17 @@ import express from "express";
 const router = express.Router();
 import register from "../controllers/user/register.js";
 import login from "../controllers/user/login.js";
+import restaurantRouter from "./restaurant.js";
+import reservationRouter from "./reservation.js";
 
+// User routes
 router.post('/register-user', register);
 router.post('/login-user', login);
+
+// Restaurant routes
+router.use('/restaurant', restaurantRouter);
+
+// Reservation routes
+router.use('/reservation', reservationRouter);
 
 export default router;
