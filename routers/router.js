fea93b2dc0bbd4e5d74b0341fd957bb4d0f257af
abhishek-2k23 <kumarabhishek00090@gmail.com>
2025-04-08 21:16:07 +0530

    update routes according to the project need

--- routers/router.js
+++ routers/router.js
@@ -1,18 +1,32 @@
 import express from "express";
 const router = express.Router();
+
+//user
 import register from "../controllers/user/register.js";
 import login from "../controllers/user/login.js";
-import restaurantRouter from "./restaurant.js";
-import reservationRouter from "./reservation.js";
+
+//reservation
+import userAuthentication from "../middleware/validators/userAuthentication.js";
+import reservationValidator from "../middleware/validators/reservationValidator.js";
+import createReservation from "../controllers/reservation/createReservation.js";
+import checkReservationDetails from "../controllers/reservation/checkReservationDetails.js";
+import cancelReservation from "../controllers/reservation/cancelReservation.js";
+
+//restaurant
+import fetchRestaurantData from "../controllers/Restaurant/fetchRestaurantData.js";
+import addRestaurant from "../controllers/restaurant/addRestaurant.js";
 
 // User routes
 router.post('/register-user', register);
 router.post('/login-user', login);
 
 // Restaurant routes
-router.use('/restaurant', restaurantRouter);
+router.get('/restaurant', fetchRestaurantData);
+router.post('/addRestaurant', reservationValidator,addRestaurant)
 
 // Reservation routes
-router.use('/reservation', reservationRouter);
+router.post('/reserve', userAuthentication, reservationValidator, createReservation);
+router.get('/reservations', userAuthentication, checkReservationDetails );
+router.put('/reservations/:id', userAuthentication, cancelReservation);
 
 export default router;
